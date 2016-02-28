7c1dd28455caa9a18167ef25c59c5900b0744f4e
lukasi273 <lukasani2273@gmail.com>
2016-02-28 01:37:40 +0100

    Trying higher range on E?

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Quinn.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Quinn.cs
@@ -21,7 +21,7 @@ namespace OneKeyToWin_AIO_Sebby
         public void LoadOKTW()
         {
             Q = new Spell(SpellSlot.Q, 1000);
-            E = new Spell(SpellSlot.E, 700);
+            E = new Spell(SpellSlot.E, 825);
             W = new Spell(SpellSlot.W, 2100);
             R = new Spell(SpellSlot.R, 550);
 
